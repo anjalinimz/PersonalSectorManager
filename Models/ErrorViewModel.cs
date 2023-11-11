aa97cc26fa03d4d6c82029520d17f5d7bddb72ec
“Anjali <anjalinimz@gmail.com>
2023-11-11 19:09:20 +0200

    Removed unwanted HTML files and fixed index html deficiencies

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PersonalSectorManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
