4ec0cb5ec4254a8033a15ec18303f58287573654
“Anjali <anjalinimz@gmail.com>
2023-11-11 19:29:22 +0200

    Added Sector controller and service

---
+++ Controllers/SectorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalSectorManager.Controllers
+{
+    public class SectorController : Controller
+    {
+        private readonly ILogger<SectorController> _logger;
+
+        public SectorController(ILogger<SectorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //call and retrieve sectorList from SectorService
+            //Return the list to View
+            return View();
+        }
+    }
+}
+
---
+++ Service/SectorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using PersonalSectorManager.Models;
+
+namespace PersonalSectorManager.Service
+{
+    public class SectorService
+    {
+        private readonly PersonalSectorDBContext _context;
+
+        public SectorService(PersonalSectorDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
+
